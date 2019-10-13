1082fc0653132823cdbdd9db26e6843fd2315bec
octa8888 <malvin00.com@gmail.com>
2019-10-13 19:03:57 +0700

    masukkan register, login dan index. Untuk login dan Index belum selesai

---
+++ controller/registerController.php
@@ -0,0 +1,37 @@
+<?php
+
+include('../database/connect.php');
+
+if(isset($_POST['submitButton'])){
+    $username=$_POST['username'];
+    $email=$_POST['email'];
+    $dob=$_POST['dob'];
+    $password=$_POST['password'];
+    $password2=$_POST['password2'];
+    
+    if(empty($username)){
+        header("location: ../register.php?error=1");
+    }
+    else if(empty($email)){
+        header("location: ../register.php?error=2");
+    }
+    else if(empty($dob)){
+        header("location: ../register.php?error=3");
+    }
+    else if(empty($password)){
+        header("location: ../register.php?error=4");
+    }
+    else if($password!==$password2){
+        header("location: ../register.php?error=5");
+    }
+    else{
+        // echo "Register";
+        $userid=md5($username.$password);
+        $query=$con->prepare("INSERT INTO KUser VALUES(?,?,?,?,?)");
+        $query->bind_param("sssss",$userid,$username,$password,$dob,$email);
+        $query->execute();
+        header("location: ../index.html");
+    }
+    return;
+}
+header("location: ../register.php?");
---
+++ register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link rel="stylesheet" href="styles/register.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Register</h1>
+        <form action="controller/registerController.php" method="POST" class="form">
+            Username
+            <input type="text" name="username">
+            Email
+            <input type="text" name="email">
+            Date of Birth
+            <input type="date" name="dob">
+            Password
+            <input type="password" name="password">
+            Retype your password
+            <input type="password" name="password2">
+            <input type="submit" value="Register" class="submitBtn" name="submitButton">
+            <div>
+                <?php
+                    if(isset($_GET['error'])){
+                        if($_GET['error']==1){
+                            echo "Username must be filled !";
+                        }
+                        else if($_GET['error']==2){
+                            echo "Email must be filled !";
+                        }
+                        else if($_GET['error']==3){
+                            echo "Must input Date of Birth !";
+                        }
+                        else if($_GET['error']==4){
+                            echo "Password must be filled !";
+                        }
+                        else if($_GET['error']==5){
+                            echo "Password is not valid !";
+                        }
+                    }
+                ?>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
