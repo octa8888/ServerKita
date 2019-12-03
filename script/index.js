cef0d5d011a51a87e241b08a04b53266410fd10a
octa8888 <malvin00.com@gmail.com>
2019-12-03 14:43:05 +0700

    tambahkan slider

---
+++ script/index.js
@@ -0,0 +1,22 @@
+var idx=0;
+
+function showSlide(n){
+    var slide=document.getElementsByClassName("slider-picture");
+    for(let i=0;i<slide.length;i++){
+        slide[i].style.display="none";
+    }
+    slide[n].style.display="block";
+}
+
+function slideAuto(maxSlide){
+    if(idx>maxSlide){
+        idx=0;
+    }
+    showSlide(idx);
+    idx++;
+}
+
+showSlide(0);
+
+setInterval(slideAuto,2500,1);
+
