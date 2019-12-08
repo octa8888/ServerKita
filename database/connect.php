140c467d8e7c894d7fec63f71f04324ad486eee9
octa8888 <malvin00.com@gmail.com>
2019-12-08 19:34:43 +0700

    connect

--- database/connect.php
+++ database/connect.php
@@ -1,9 +1,9 @@
 <?php
     
-// $con= new mysqli('localhost','phpmyadmin','Serveryangpernahada123!','db_kita');
-try{
-    $con= new mysqli('localhost','root','','db_kita');
-}catch(Exception $e){
-    header("location: ../index.php");
-    die();
-}
+$con= new mysqli('localhost','phpmyadmin','Serveryangpernahada123!','db_kita');
+// try{
+//     $con= new mysqli('localhost','root','','db_kita');
+// }catch(Exception $e){
+//     header("location: ../index.php");
+//     die();
+// }
