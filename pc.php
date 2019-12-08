f6d7586b8532c542010417a5819837e63990ccb2
octa8888 <malvin00.com@gmail.com>
2019-12-08 17:16:41 +0700

    jualan

--- pc.php
+++ pc.php
@@ -1,9 +1,13 @@
+<?php
+    include("database/connect.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="./styles/pc.css">
     <title>PC</title>
 </head>
@@ -11,12 +15,45 @@
     <?php
         include('header.php');
     ?>
-    <div class="main-page">
-        <div class="classA">A</div>    
-        <div class="classB">B</div>    
-        <div class="classC">C</div> 
-        <div class="classD">D</div> 
+    <script src="script/jquery.min.js"></script>
+    
+    <div class="container">
+        
+        
+
+
+         <?php
+            $query="SELECT * FROM KItems";
+            if($rs=$con->query($query)){
+                while($data=$rs->fetch_assoc()){
+        ?>
+            <div class="row">
+                <div class="col-lg-3 shop-item">
+                    <img src="<?=$data['Picture']?>" class="img-setting">
+                    <div class="description">
+                        <h5><?=$data['Name']?></h5>
+                        <div>
+                            <?=$data['Description']?>
+                        </div>
+                        <div style="margin-top: 10px; color:orange">
+                            <strong>Rp. <?=$data['Price']?></strong>
+                        </div>
+                    </div>
+                    <div class="cart-button">
+                        <button class="btn btn-danger">Buy</button>
+                    </div>
+                </div>
+            </div>
+
+        <?php  
+                } 
+            }
+        ?>
+
+        
+
     </div>
+    
 
 </body>
 </html>
